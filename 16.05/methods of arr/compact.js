571c869436e9ba9305379dd736664c56b7b4ec79
ungsinatra <mamirov.21@mail.ru>
2022-05-15 20:39:35 +0300

    campactArr

---
+++ 16.05/methods of arr/compact.js
@@ -0,0 +1,11 @@
+// Compact. Напишите функцию, которая очищает массив от нежелательных значений, таких как false, undefined, пустые строки, ноль, null.
+
+const compact = (array) => {
+	let newArr=array.filter((el) => {
+	return Boolean(el)
+  });
+  return newArr
+};
+
+const data = [0, 1, false, 2, undefined, "", 3, null];
+console.log(compact(data)); // [1, 2, 3]
