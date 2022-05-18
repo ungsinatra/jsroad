18f225b52f803df1d3df5bfae604c587340c1f30
ungsinatra <mamirov.21@mail.ru>
2022-05-18 14:51:46 +0300

    closure not done all

---
+++ chapter 6/shortCircuit/short1Start.js
@@ -0,0 +1,54 @@
+// let a  = 0;
+
+// function one(){
+// 	let = 0;
+// 	return function(){
+// 		a=a+1;
+// 		return a
+
+// 	}
+// }
+
+// let startOne  = one();
+
+
+
+// let a = 12;
+// const two = ()=>{
+// 	let a = 12;
+// 	let b = 'sardor'
+// 	return  ()=>{
+// 		return	console.log(`${b} ${a}`);
+		
+// 	}
+
+// }
+
+// const twoFc= two();
+
+// console.log(twoFc);
+// console.log(twoFc());
+
+
+// counter
+// function counter (first){
+// 	let valueCounter = first;
+// 	return function(){
+// 		return valueCounter++;
+
+// 	}
+
+// }
+
+// const firstCounter = counter(1);
+// const secondCounter = counter(1);
+
+
+
+// console.log(secondCounter());
+// console.log(secondCounter());
+// console.log(secondCounter());
+// console.log(secondCounter());
+// console.log(secondCounter());
+// 
+
