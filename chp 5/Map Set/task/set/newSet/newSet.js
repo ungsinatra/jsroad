20237dc09091f9faf25f40d2ebe0c6f6d4cb67e9
ungsinatra <mamirov.21@mail.ru>
2022-05-21 16:34:30 +0300

    destrict tusk done

--- chp 5/Map Set/task/set/newSet/newSet.js
+++ chp 5/Map Set/task/set/newSet/newSet.js
@@ -111,7 +111,7 @@ console.log(tottal)//60
 Задача 7:
 
 Дан инпут и кнопка. Пользователь вводит в инпут число и жмет на кнопку и делает так произвольное количество раз. Сделайте так, чтобы каждый раз под инпутом в абзац через запятую выводился список всех введенных пользователем чисел, но без дублей.
-*/
+
 
 const set = new Set();
 let  text= document.querySelector('#p')
@@ -123,3 +123,138 @@ document.querySelector('#button').onclick = ()=>{
 	
 }
 console.log(str)
+
+*/
+
+/*
+
+Пусть дан массив. С помощью Set получите этот же массив, но без дублей.
+
+
+function getUniqe(arr){
+	let set  = new Set(arr)
+
+	let setArr = Array.from(set)
+
+
+	return  setArr;
+
+}
+
+
+console.log(getUniqe([1,2,3,4,5,3,2,45]))
+
+
+*/
+
+/*
+Даны абзацы и кнопка. Пользователь кликает на эти абзацы в произвольном порядке. Сделайте так, чтобы по нажатию на кнопку в конец каждого абзаца, на который был совершен клик, был добавлен восклицательный знак.
+
+let newSet = new Set();
+let p  = document.querySelectorAll('p');
+
+
+for(let char of p){
+	char.addEventListener('click',function(){
+		newSet.add(this)
+	
+
+	})
+
+	
+}
+
+
+
+let bnt  = document.querySelector('button');
+bnt.addEventListener('click',()=>{
+
+	for(let char of newSet){
+		char.textContent+='!';
+		
+		
+	}
+	
+})
+
+*/
+
+
+// Map
+
+/*
+Задача :
+
+Пусть даны 3 массива. Создайте коллекцию Map, сделайте ключами коллекции эти массивы, а значениями - какие-нибудь строк
+
+
+
+let arr  = [1,2,3]
+let arr2  = [1,2,3]
+let arr3  = [1,2,3]
+let newMap = new Map()
+newMap.set(arr,'1')
+newMap.set(arr2,'2')
+newMap.set(arr3,'3')
+
+console.log(newMap)
+
+*/
+
+
+/* Переберите циклом for-of коллекцию, созданную в предыдущей задаче.
+
+for(let char of newMap){
+	console.log(char);
+
+}
+
+*/
+
+
+/*
+Пусть дана коллекция Map. С помощью метода keys получите массив ее ключей и переберите его циклом for-of
+Пусть дана коллекция Map. С помощью метода values получите массив ее значений и переберите его циклом for-of.
+
+
+for(let char of newMap.keys()){
+	console.log(char)
+	
+}
+
+for(let char of newMap.values()){
+	console.log(char)
+	
+}
+*/
+
+
+/*
+Задача :
+
+Пусть даны абзацы. Переберите эти абзацы циклом и создайте коллекцию Map, ключами в которой будут абзацы, а значением - их порядковый номер на странице. Сделайте так, чтобы по клику на любой абзац ему в конец записывался его порядковый номер.
+
+
+
+
+let p1 = document.querySelectorAll('p');
+let map = new Map();
+
+let count = 1;
+for(let char of p1){
+	map.set(char,count);
+	count+=1;
+	for(let el of map.values()){
+		console.log(el)
+		char.addEventListener('click',()=>{
+			char.textContent += el
+		
+		})
+	}
+
+}
+console.log(map)
+
+
+
+*/
