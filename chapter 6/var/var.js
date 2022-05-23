95bda1891075382ee9c39f6605ef1ee46bc13d03
ungsinatra <mamirov.21@mail.ru>
2022-05-23 18:44:34 +0300

    var train

---
+++ chapter 6/var/var.js
@@ -0,0 +1,81 @@
+/*
+function getVar(){
+	console.log(getVar);
+	//undefined
+	var getVar;
+	getVar = 'я VAR'
+
+}
+getVar()
+*/
+
+/*
+function getVar(){
+	
+	getVar = 'я VAR'
+	console.log(getVar);//'я var'
+	var getVar;
+
+}
+getVar()
+*/
+/*
+
+
+if(true){
+	var num = 15
+}
+
+console.log(num)//15
+*/
+
+/*
+var number = 15;
+console.log(number)//15
+var number = 20;
+console.log(number)//20
+
+*/
+/*
+let speed  = 15;
+var straph = false
+ 
+ if (speed >10){
+	 straph = true
+ }
+ console.log(straph)//true
+
+ */
+
+/*
+function sayhi() {
+  phrase = "Hi";
+
+  console.log(phrase);//hi
+
+  var phrase;
+  function getNm() {
+    phrase = 14;
+  }
+  getNm();
+  console.log(phrase);//14
+}
+
+sayhi();
+*/
+
+
+function sayhi() {
+	
+
+	 function newVar(){
+		var num = 14
+	}
+	newVar()
+	num = 14
+	console.log(num)//14
+	var num
+
+  }
+  
+  sayhi();
