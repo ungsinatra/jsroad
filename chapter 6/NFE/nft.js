0103202088f71ea8434c92a116d4781ba501a9fb
ungsinatra <mamirov.21@mail.ru>
2022-05-23 22:16:48 +0300

    nfe

---
+++ chapter 6/NFE/nft.js
@@ -0,0 +1,51 @@
+
+/*
+function getName(){
+	console.log('hi')
+
+
+}
+
+console.log(getName.name)
+*/
+
+/*
+const newArr =()=>{
+	return console.log('elena')
+
+}
+
+console.log(newArr.name)
+*/
+
+/*
+function f2(sayhi = function(){}){
+	console.log(sayhi.name);
+
+}
+
+console.log(f2())
+
+*/
+
+/*
+function makeCounter() {
+
+	function counter() {
+	  return counter.count++;
+	};
+  
+	counter.count = 0;
+  
+	return counter;
+  }
+  
+  let counter = makeCounter();
+  
+  counter.count = 10;
+  console.log( counter() ); // 10
+  console.log( counter() ); // 10
+  console.log( counter() ); // 10
+  console.log( counter() ); // 10
+
+*/
