ee7fc93b57ff5a4919496a86c776d2d2b43bd306
ungsinatra <mamirov.21@mail.ru>
2022-05-24 19:10:48 +0300

    newFc done

---
+++ chapter 6/newFunction/newFn.js
@@ -0,0 +1,14 @@
+
+
+/*
+const getNew = new Function('a,b','return a+b')
+console.log(getNew(13,12))//25
+*/
+
+
+/*
+const newFc = new Function('console.log("Привет")')
+console.log(newFc())//привет 
+*/
+
+
