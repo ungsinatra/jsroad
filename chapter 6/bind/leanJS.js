b4e285e1ba9ac6c4c67c330dc2213957101c5ad4
ungsinatra <mamirov.21@mail.ru>
2022-06-19 20:45:23 +0300

    not done

---
+++ chapter 6/bind/leanJS.js
@@ -0,0 +1,40 @@
+
+function askPassword(ok, fail) {
+	let password = prompt("Password?", '');
+	if (password == "rockstar") ok();
+	else fail();
+  }
+  
+  let user = {
+	name: 'Вася',
+  
+	loginOk() {
+	  alert(`${this.name} logged in`);
+	},
+  
+	loginFail() {
+	  alert(`${this.name} failed to log in`);
+	},
+  
+  };
+  
+  askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+
+
+// ==============================================================
+
+function askPassword(ok, fail) {
+	let password = prompt("Password?", '');
+	if (password == "rockstar") ok();
+	else fail();
+  }
+  
+  let user = {
+	name: 'John',
+  
+	login(result) {
+	  alert( this.name + (result ? ' logged in' : ' failed to log in') );
+	}
+  };
+  
+  askPassword(?, ?); // ?
