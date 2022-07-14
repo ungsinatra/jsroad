701555a138ca443aa16dc4072edbec59f3dca397
ungsinatra mac <mamirov.21@mail.ru>
2022-07-14 12:30:25 +0300

    f explored

--- chapter8/f.prototype/F.explore.js
+++ chapter8/f.prototype/F.explore.js
@@ -10,9 +10,8 @@ function Rabbit(name){
 Rabbit.prototype = animal;
 let rabbit = new Rabbit('black rabbit');
 
-console.log(rabbit.__proto__);
 console.log(Rabbit.prototype.constructor == Rabbit)
-console.log(rabbit.__proto__ == animal)
+console.log(rabbit.__proto__ == animal)//true
 
 //===========================
 
@@ -21,8 +20,37 @@ function Car(name){
     this.name = name;    
 }
 
-
+console.log(Car.__proto__ == Function.prototype)//true
 let tesla = new Car('tesla');
+console.log(tesla.__proto__ == Car.prototype)//true
+
+console.log( Car.prototype.constructor==Car)//true
 
 
-console.log(Car.prototype)
+//==========================================
+
+
+let BMW = new Car('BMW M2');
+let FERRARI = new BMW.constructor('FERRARI F40)');
+
+console.log(BMW.__proto__ == Car.prototype)//true
+console.log(FERRARI.__proto__ == Car.prototype)//true
+
+
+
+
+//============
+
+
+Car.prototype.drive = function(){
+    console.log('driving...')
+}
+
+
+BMW.drive()
+tesla.drive()
+FERRARI.drive()
+
+
+
+//===================
