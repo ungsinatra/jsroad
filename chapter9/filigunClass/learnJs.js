764dc42834fcd47109c8b5fe5c6f13c5e5604157
ungsinatra Laptop <mamirov.21@mail.ru>
2022-08-04 08:23:53 +0300

    learnJs done

---
+++ chapter9/filigunClass/learnJs.js
@@ -0,0 +1,74 @@
+
+class Animal {
+
+    constructor(name) {
+      this.name = name;
+    }
+  
+  }
+  
+  class Rabbit extends Animal {
+    constructor(name) {
+    //   this.name = name;
+      super(name);
+      this.created = Date.now();
+    }
+  }
+  
+  let rabbit = new Rabbit("Белый кролик"); // Error: this is not defined // теперь все ок
+  alert(rabbit.name);
+
+  
+
+class Clock {
+    constructor({ template }) {
+      this.template = template;
+    }
+  
+    render() {
+      let date = new Date();
+  
+      let hours = date.getHours();
+      if (hours < 10) hours = '0' + hours;
+  
+      let mins = date.getMinutes();
+      if (mins < 10) mins = '0' + mins;
+  
+      let secs = date.getSeconds();
+      if (secs < 10) secs = '0' + secs;
+  
+      let output = this.template
+        .replace('h', hours)
+        .replace('m', mins)
+        .replace('s', secs);
+  
+      console.log(output);
+    }
+  
+    stop() {
+      clearInterval(this.timer);
+    }
+  
+    start() {
+      this.render();
+      this.timer = setInterval(() => this.render(), 1000);
+    }
+  }
+
+
+
+class ExtendedClock extends Clock{
+    constructor({template,pressision = 1000}){
+        super({template});
+        this.pressision = pressision;
+    }
+    start(){
+        super(this.render);
+        this.timer = setInterval(this.render,this.pressision)
+    }
+}
+const tmp = {
+     content: 'hh:mm:ss' 
+}
+newClock = new ExtendedClock(tmp);
+newClock.start()
