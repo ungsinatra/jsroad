da0573204563ec4dc4a165325754b73f2626d80d
ungsinatra Laptop <mamirov.21@mail.ru>
2022-08-09 23:51:56 +0300

    learn js

---
+++ chapter9/TypeOfVAr/learnJS.js
@@ -0,0 +1,9 @@
+class Rabbit extends Object{
+    constructor(name){
+        super()
+        this.name = name
+    }
+}
+hasOwnProperty
+const newRAbbit = new Rabbit('bad');
+console.log(newRAbbit.hasOwnProperty('name'));
